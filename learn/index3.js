e53fc826aca3685c5a2495f66cf48091b2e5ae6f
Pragya.Dubey <Pragya.Dubey@ril.com>
2024-06-21 15:35:22 +0530

    test commit

--- learn/index3.js
+++ learn/index3.js
@@ -1,5 +1,6 @@
 let fullname = prompt("ENter the fullname");
 let start = "@";
 let end = fullname.length;
-let result = start+fullname+end;
+let result = start + fullname + end;
 document.write("the username is " + result);
+console.log("hello");
